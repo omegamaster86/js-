3bcf37ec81fcfecddc614dc87ccfdbcc642cdd37
蒲原正俊 <kanbaramasatoshi@kanbaramasatoshinoMacBook-Pro.local>
2022-12-21 15:16:10 +0900

    ifも使用して作成致しました

--- app.js
+++ app.js
@@ -1,10 +1,11 @@
 const Name = prompt("日本の首都は？");
 
-while (Name == "東京") {
-    alert ("正解です");
-    break
-} 
-while (Name !== "東京") {
-    alert ("不正解です");
-    break
+while (Name.includes("")) {
+    if (Name == "東京") {
+        alert ("正解です");
+        break
+    }  else {
+        alert ("不正解です");
+        break
+    }
 }
