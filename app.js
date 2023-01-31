40afe20ee2d9b77b5a736a5c98c0421af0936c95
蒲原正俊 <kanbaramasatoshi@kanbaramasatoshinoMacBook-Pro.local>
2023-02-01 00:17:30 +0900

    JS課題1-4終了しました。

--- app.js
+++ app.js
@@ -1,14 +1,21 @@
-const tasks = ['0 : 掃除','1 : 買い物','2 : 散歩'];
+const tasks = ['0 :[内容]机を片付ける、[ジャンル] 掃除','1 :[内容]牛乳を買う、[ジャンル]買い物','2 :[内容]散歩する、[ジャンル]運動'];
+const newTask1 = prompt("タスク入力して下さい");
+//const newTask2 = prompt("ジャンルを入力してください");//こいつが原因
 
 console.log("=======================\n現在持っているタスク一覧\n=======================");
 for (const todo of tasks) {
     console.log(todo);
 }
 
-const newTask = prompt("タスク入力して下さい");
-tasks.push('3 : ' + newTask);
+function fn () {
+    if (newTask1 !== null) {
+        const newTask2 = prompt("ジャンルを入力してください");
+        tasks.push('3 :[内容]' + newTask1 + '、' + '[ジャンル]' + newTask2);
+    }
+}
+fn();
 
-if (newTask !== null) {
+if (newTask1 !== null) {
     alert("新しいタスクを追加しました。");
 } 
 
@@ -17,6 +24,6 @@ for (let todo of tasks) {
     console.log(todo);
 }
 
-if (newTask !== null) {
-    alert("「確認、追加、削除、終了」の４つのいずれかを入力してください。")
+if (newTask1 !== null) {
+    alert("「確認、追加、削除、終了」の４つのいずれかを入力してください。");
 }
